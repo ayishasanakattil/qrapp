707f71b6c51a594019d94f514e3808de45f13b2a
idhanush <idhanush.m@gmail.com>
2024-03-12 17:04:35 +0530

    123

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:qrapplication/REGISTER.dart';
 import 'package:qrapplication/login.dart';
+import 'package:qrapplication/REGISTER.dart';
 
 void main() {
   runApp(MaterialApp(
